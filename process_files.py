f19ae4394202b0a0a0e3e4c430d7cc643a00a5cb
Gentry Atkinson <gma23@txstate.edu>
2020-02-04 13:04:54 -0600

    working on file processors

---
+++ process_files.py
@@ -0,0 +1,12 @@
+########################################
+#Author: Gentry Atkinson
+#Date: 04 Feb 2020
+#Organization: Texas State University
+#Description: read the MNIST files into CSVs
+########################################
+
+def main():
+
+
+if __name__ == "__main__":
+    main()
