821034156669389c8d49aea8c6c36ad894d346a7
Gentry Atkinson <gma23@txstate.edu>
2020-02-05 15:28:20 -0600

    starting work on the lable swapper

---
+++ mislable_data.py
@@ -0,0 +1,6 @@
+########################################
+#Author: Gentry Atkinson
+#Date: 05 Feb 2020
+#Organization: Texas State University
+#Description: randomly alter several labels in the MNIST CSVs
+########################################
