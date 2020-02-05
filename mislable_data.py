e6824420a9c648abe6b3c89e4c968ed2ca3ce73c
Gentry Atkinson <gma23@txstate.edu>
2020-02-05 17:47:15 -0600

    added seed to mislabeler

--- mislable_data.py
+++ mislable_data.py
@@ -19,28 +19,33 @@ NUM_TEST_SAMPLES = 10000
 
 import numpy as np
 import random as rand
+from datetime import datetime
 
 def main():
     train_labels = np.loadtxt(PLAIN_TRAIN_LABELS, delimiter=',')
     altered_labels = open(ALTERED_TRAIN_LABELS, "w+")
     altered_indexes = open(ALTERED_TRAIN_LABELS_INDEXES, "w+")
-    rand.seed()
+    rand.seed(datetime.now())
     index = -1
+    count = 0
     for label in train_labels:
         index = index + 1
         label = int(label)
-        if rand.randint(0,100)<PERCENT_TO_ALTER:
-            newLabel = rand.randint(0,10)
+        picker = rand.randint(0,100)
+        if picker < PERCENT_TO_ALTER:
+            newLabel = rand.randint(0,9)
             while newLabel == label:
-                newLabel = rand.randint(0,10)
+                newLabel = rand.randint(0,9)
             altered_labels.write(str(newLabel))
             altered_labels.write("\n")
             altered_indexes.write(str(index))
             altered_indexes.write("\n")
+            count += 1
             print("Index ", index, " changed from ", label, " to ", newLabel)
         else:
             altered_labels.write(str(label))
             altered_labels.write("\n")
+    print(count, " total entries changed")
 
 
 if __name__ == "__main__":
