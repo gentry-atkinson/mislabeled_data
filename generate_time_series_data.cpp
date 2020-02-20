e1de0a42655746e8a2c2c8f34e99c27457fff0cf
Gentry Atkinson <gma23@txstate.edu>
2020-02-20 10:46:18 -0600

    added label counter to data maker

--- generate_time_series_data.cpp
+++ generate_time_series_data.cpp
@@ -4,7 +4,7 @@
 *Organization: Texas State Univerity
 *Description: this command line tool will generate simple sets of synthetic
 *             labeled time-series data.
-*******************************************************************************/ 
+*******************************************************************************/
 
 #include<iostream>
 #include<fstream>
@@ -34,19 +34,22 @@ int main(int argc, char** argv){
   float signal2Scale[numLables], signal2Phase[numLables], signal2Offset[numLables];
   float signal3Scale[numLables], signal3Phase[numLables], signal3Offset[numLables];
 
+  int labelCounts[numLables];
+
   srand(time(NULL));
 
   //initialze signal values for each label
   for(int i = 0; i < numLables; i++){
     signal1Scale[i] = 0.5 + rand()/maxInt;
     signal1Phase[i] = rand()/maxInt;
-    signal1Offset[i] = rand()%5;
+    signal1Offset[i] = rand()/maxInt - 0.5;
     signal2Scale[i] = 1.0 + 2*rand()/maxInt;
     signal2Phase[i] = 2*rand()/maxInt;
-    signal2Offset[i] = rand()%5;
+    signal2Offset[i] = 2.0*rand()/maxInt - 1.0;
     signal3Scale[i] = 4.0 + 3*rand()/maxInt;
-    signal3Phase[i] = 2*rand()/maxInt;
-    signal3Offset[i] = rand()%5;
+    signal3Phase[i] = 3*rand()/maxInt;
+    signal3Offset[i] = 4.0*rand()/maxInt - 2.0;
+    labelCounts[i] = 0;
   }
 
   //write samples
@@ -56,7 +59,7 @@ int main(int argc, char** argv){
     scaleNoise = 0.1 * rand()/maxInt + 1.0;
     phaseNoise = 0.1 * rand()/maxInt;
     for(int j = 0; j<sampleRate*numSeconds; j++){
-        offsetNoise = 0.05 * rand()/maxInt;
+        offsetNoise = 0.2 * rand()/maxInt-0.1;
         float sampleValue = 0;
         sampleValue += sin(((2.0*j/static_cast<float>(sampleRate))*scaleNoise*signal1Scale[sampleLable]+signal1Phase[sampleLable]+phaseNoise)*M_PI)+offsetNoise+signal1Offset[sampleLable];
         sampleValue += sin(((2.0*j/static_cast<float>(sampleRate))*scaleNoise*signal2Scale[sampleLable]+signal2Phase[sampleLable]+phaseNoise)*M_PI)+offsetNoise+signal2Offset[sampleLable];
@@ -66,6 +69,13 @@ int main(int argc, char** argv){
     }
     labelFile << sampleLable << endl;
     sampleFile << endl;
+    labelCounts[sampleLable]++;
+  }
+
+  //print summary information
+  cout << "Number of samples from: " << endl;
+  for(int i = 0; i < numLables; i++){
+    cout << "Label " << i << "\t\t" << labelCounts[i] << endl;
   }
 
   return 0;
