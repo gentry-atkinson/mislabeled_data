bc71517168f506acb77a391a2604a71fc4626f7d
Gentry Atkinson <gma23@txstate.edu>
2020-02-26 19:42:50 -0600

    added a confusion matrix

--- count_C.m
+++ count_C.m
@@ -5,13 +5,44 @@
 %C -> the results of clustering SPrimePrime
 
 
-number_of_clusters = max(C) + 1;
-C_counts = zeros(number_of_clusters);
+number_of_clusters = max(C);
+disp(number_of_clusters);
+disp(max(size(C)));
+C_counts = zeros(number_of_clusters,1);
 
-for i = 1:size(C)
-   if C(i) == -1
-       C_counts(0) = C_counts(0)+1;
-   else
+for i = 1:max(size(C))
+   if C(i) ~= -1
        C_counts(C(i)) = C_counts(C(i))+1;
    end
 end
+
+%1 is being used as a flag for a mislabeled cluster
+mislabeled_clusters = zeros(number_of_clusters, 1);
+for i = 1:number_of_clusters
+   if C_counts(i) < minPts
+       mislabeled_clusters(i) = 1;
+   end
+end
+
+mislabeled_indexes = [];
+for i = 1:max(size(C))
+   if C(i) == -1
+       mislabeled_indexes = [mislabeled_indexes; i];
+   elseif mislabeled_clusters(C(i)) == 1
+       mislabeled_indexes = [mislabeled_indexes; i];
+   end
+end
+
+%1 is used to flag a mislabeled data point
+real_mislabeled_list = zeros(max(C), 1);
+predicted_mislabeled_list = zeros(max(C), 1);
+
+
+for i = 1:size(alterindexes, 1)
+      real_mislabeled_list(alterindexes(i)) = 1;
+end
+for i = 1:size(mislabeled_indexes, 1)
+      predicted_mislabeled_list(mislabeled_indexes(i)) = 1;
+end
+
+confusion_matrix = confusionmat(real_mislabeled_list, predicted_mislabeled_list);
