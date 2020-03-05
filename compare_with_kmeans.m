7f9e3f2e63da68c0c55f0f62b5cd3c3bc63c9f7c
Gentry Atkinson <gma23@txstate.edu>
2020-03-05 11:17:58 -0600

    trying kmeans comparison

---
+++ compare_with_kmeans.m
@@ -0,0 +1,7 @@
+%Author: Gentry Atkinson
+%Date: 26 Feb 2020
+%Organization: Texas State University
+%
+%Inputs: syntheticfeatures, sprimeprime, and numLabels
+
+[C, centers] = kmeans(syntheticfeatures, 2);
