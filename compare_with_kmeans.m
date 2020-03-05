de354f3f16cebc7d248a3eb7ac3551e319fedcdb
Gentry Atkinson <gma23@txstate.edu>
2020-03-05 13:23:47 -0600

    changed kmeans algorithm

--- compare_with_kmeans.m
+++ compare_with_kmeans.m
@@ -2,37 +2,61 @@
 %Date: 26 Feb 2020
 %Organization: Texas State University
 %
-%Inputs: syntheticfeatures, syntheticsprimeprime, alterindexes, and numLabels
+%Inputs: syntheticfeatures, syntheticsprimeprime_normal, alterindexes, and numLabels
+
+tolerance = 0.10;
 
 feature_length = size(syntheticfeatures(1,:), 2);
 number_samples = size(syntheticfeatures, 1);
 
-distance = zeros(numLabels, 1);
+feat_norm = normalize(syntheticfeatures);
+%spp_norm = normalize(syntheticsprimeprime);
+
+distance = zeros(number_samples, 2);
 mislabels_observed = zeros(number_samples, 1);
 mislabels_predicted = zeros(number_samples, 1);
-syntheticfeatures_padded = zeros(number_samples, feature_length+numLabels);
+feat_norm_padded = zeros(number_samples, feature_length+numLabels);
 
 %pad the feature array with zeros for lables
 for i = 1:number_samples
     for j = 1:feature_length
-        syntheticfeatures_padded(i, j) = syntheticfeatures(i,j);
+        feat_norm_padded(i, j) = feat_norm(i,j);
     end
     for k = 1:numLabels
-       syntheticfeatures_padded(i, feature_length+k) = 0;
+       feat_norm_padded(i, feature_length+k) = 0;
     end
 end
 
 %compute cluster with k=numLabels
-[C, centers] = kmeans(syntheticfeatures_padded, numLabels, 'Distance', 'cosine');
+[C, centers, sums, dis_to_all_clusters] = kmeans(feat_norm_padded, numLabels, 'Distance', 'cosine');
 
-%find points in sprimeprime which are closer to the wrong cluster now
+%flag values: 0=correct, 1=mislabeled
+
+%find who's distance has moved the most
 for i = 1:number_samples
-    for j = 1:numLabels
-        distance(j) = getCosineSimilarity(syntheticsprimeprime(i,:), centers(j,:));
-    end
-    dis_to_my_cluster = getCosineSimilarity(syntheticsprimeprime(i,:), centers(C(i),:));
-    if min(distance) ~= dis_to_my_cluster
-       mislabels_predicted = 1; 
-    end
+    %dis_to_my_cluster = 1-getCosineSimilarity(syntheticsprimeprimenormalized(i,:), centers(C(i),:));
+    %dis_to_my_cluster = sqrt(sum((syntheticsprimeprimenormalized(i,:) - centers(C(i),:)) .^ 2));
+    
+    dis_to_my_cluster = 1-getCosineSimilarity(syntheticsprimeprimenormalized(i,:), centers(C(i),:));
+    %if abs(dis_to_my_cluster-dis_to_all_clusters(i,C(i))) > tolerance*(abs(dis_to_all_clusters(1)-dis_to_all_clusters(2)))
+    %   mislabels_predicted(i) = 1;
+    %end
+    
+    distance(i, 1) = abs(dis_to_my_cluster - dis_to_all_clusters(C(i)));
+    distance(i,2) = i;
         
 end
+
+distance = sortrows(distance,1, 'descend');
+
+for i = 1:(tolerance * number_samples)
+    mislabels_predicted(distance(i,2)) = 1;
+end
+
+%use alterindexes to set flags in mislabels_observed
+for i = 1:size(alterindexes, 1)
+   mislabels_observed(alterindexes(i)) = 1; 
+end
+
+
+confusion_matrix = confusionmat(mislabels_observed, mislabels_predicted);
