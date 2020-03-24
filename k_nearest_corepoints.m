47bd7bef15209d084e130b564e1a7111f459122e
Gentry Atkinson <gma23@txstate.edu>
2020-03-24 15:34:57 -0500

    starting nearest core points comparison

---
+++ k_nearest_corepoints.m
@@ -0,0 +1,9 @@
+%Author: Gentry Atkinson
+%Date: 24 March 2020
+%Organization: Texas State University
+
+%load files
+syntheticfeatures = readmatrix("synthetic_features.csv");
+syntheticmislabels = readmatrix("synthetic_mislabels.csv");
+alteredindexes = readmatrix("altered_indexes.csv");
+sprimeprime = readmatrix("synthetic_sprimeprime_normalized.csv");
