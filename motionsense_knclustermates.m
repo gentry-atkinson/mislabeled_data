1c31ad2154a63b47a1286f1718f8ebaa91c95536
Gentry Atkinson <gma23@txstate.edu>
2020-04-23 15:05:11 -0500

    filling classifier array

--- motionsense_knclustermates.m
+++ motionsense_knclustermates.m
@@ -8,8 +8,19 @@ labels = readmatrix("motionsense_labels.csv");
 alteredindexes = readmatrix("altered_indexes.csv");
 sprimeprime = readmatrix("motionsense_sprimeprime.csv");
 
-minPts = 80;
-epsilon = 0.95;
+minPts = 10;
+epsilon = 0.01;
 k = 6;
+numLabels = 6;
 
 [clusters, corePoints] = dbscan(features,epsilon,minPts,'Distance','cosine');
+
+fprintf("max cluster: %d\tmin cluster: %d\n", max(clusters), min(clusters));
+
+knn_clasifiers = {};
+for i = 1:max(clusters)
+    point_set = features(clusters==i, :);
+    label_set = mislabels(clusters==i);
+    knn_clasifiers(i) = fitcknn(point_set, label_set, 'Distance', 'cosine', 'NumNeighbors', k);
+end
+
