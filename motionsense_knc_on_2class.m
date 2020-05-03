8dc0b07d7f0bff6ed83b68fb2e050494a57d7c7c
Gentry Atkinson <gma23@txstate.edu>
2020-05-03 13:24:31 -0500

    added one hot splitter

--- motionsense_knc_on_2class.m
+++ motionsense_knc_on_2class.m
@@ -7,3 +7,17 @@ mislabels = readmatrix("motionsense_mislabels.csv");
 labels = readmatrix("motionsense_labels.csv");
 alteredindexes = readmatrix("altered_indexes.csv");
 sprimeprime = readmatrix("motionsense_sprimeprime.csv");
+
+num_classes = 6;
+
+oneHot_mislabels = zeros(num_classes, size(mislabels, 1));
+
+for i = 1:num_classes
+   for j = 1:size(mislabels,1)
+      if mislabels(j) == i-1
+          oneHot_mislabels(i,j) = 0;
+      else
+          oneHot_mislabels(i,j) = 1;
+      end
+   end
+end
